5b7a7a8ac3a1addc87c2024ed25efc5b01142298
textheadmin <textheadmin@users.noreply.github.com>
2015-01-17 02:13:22 -0600

    Response object lesson

--- app.py
+++ app.py
@@ -38,6 +38,13 @@ def path_type(value):
     print value
     return "correct"
 
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name)
+    else:
+        return "Not Found", 404
+
 
 if __name__ == "__main__":
     app.run()
